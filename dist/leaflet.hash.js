915fa15262a4eff64d8a5fbe521b8f28979105b9
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-12-01 07:58:42 -0500

    build

--- dist/leaflet.hash.js
+++ dist/leaflet.hash.js
@@ -1,202 +1,101 @@
 (function() {
 
-  L.Hash = function(map, layerControl) {
+  L.Hash = function(map) {
     var parsed,
       _this = this;
     this.map = map;
-    if (layerControl) {
-      this.layerControl = layerControl;
-    }
-    this.map.on("moveend layeradd layerremove", this.makeState, this);
+    this.map._hash = this;
+    this.map.on("moveend", this.update, this);
     if (this.lastHash !== location.hash) {
       parsed = this.parseHash(location.hash);
       if (parsed) {
         this.map.setView(parsed.center, parsed.zoom);
       }
     }
-    this.makeState();
+    this.makeState(this.getHash(), true);
     window.onpopstate = function(e) {
-      var hash;
       if ("state" in e && e.state) {
-        hash = _this.makeHash(e.state);
-        parsed = _this.parseHash(hash);
-        if (parsed) {
-          return _this.map.setView(parsed.center, parsed.zoom);
-        }
+        _this.map.setZoom(e.state.zoom);
+        return _this.map.panTo(e.state.center);
       }
     };
     return true;
   };
 
-  L.Hash.prototype = {
-    map: null,
-    lastHash: null,
-    layerControl: null,
-    getLayers: function() {
-      var base, cLayers, key, out, overlay;
-      base = void 0;
-      cLayers = void 0;
-      key = void 0;
-      overlay = void 0;
-      cLayers = this.layerControl._layers;
-      overlay = [];
-      base = void 0;
-      for (key in cLayers) {
-        if (cLayers[key].layer._leaflet_id && cLayers[key].layer._leaflet_id in this.map._layers) {
-          if (cLayers[key].overlay) {
-            overlay.push(encodeURIComponent(cLayers[key].name));
-            console.log("over " + encodeURIComponent(cLayers[key].name));
-          } else {
-            base = encodeURIComponent(cLayers[key].name);
-            console.log("base " + encodeURIComponent(cLayers[key].name));
-          }
-        }
-      }
-      out = {
-        base: base,
-        overlay: overlay
-      };
-      console.log(JSON.stringify(out));
-      return out;
-    },
-    getMapHash: function() {
-      var center, hashArray, precision, zoom;
-      center = this.map.getCenter();
-      zoom = this.map.getZoom();
-      precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
-      return hashArray = [zoom, center.lat.toFixed(precision), center.lng.toFixed(precision)];
-    },
-    getHash: function() {
-      var layers, mhash;
-      mhash = this.getMapHash();
-      if (this.layerControl) {
-        layers = this.getLayers();
-      }
-      if (!this.layerControl) {
-        layers = {};
-      }
-      layers.zoom = mhash.shift();
-      layers.center = mhash;
-      return layers;
-    },
-    makeHash: function(hashObj) {
-      var hashParts;
-      hashParts = [hashObj.zoom, hashObj.center[0], hashObj.center[1], hashObj.base];
-      if ("overlay" in hashObj) {
-        if (hashObj.overlay.length = 1) {
-          hashParts.push(hashObj.overlay[0]);
-        }
-        if (hashObj.overlay.lengh > 1) {
-          hashParts.push(hashObj.overlay.join("-"));
-        }
-      }
-      return "#" + hashParts.join("/");
-    },
-    makeState: function() {
-      var hash, layers, mhash;
-      if (!history.pushState) {
+  L.Hash.prototype.map = null;
+
+  L.Hash.prototype.lastHash = null;
+
+  L.Hash.prototype.getHash = function() {
+    var center, hash, precision, zoom;
+    center = this.map.getCenter();
+    zoom = this.map.getZoom();
+    precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
+    hash = {
+      zoom: zoom,
+      center: [center.lat.toFixed(precision), center.lng.toFixed(precision)]
+    };
+    return hash;
+  };
+
+  L.Hash.prototype.makeState = function(hash, replace) {
+    var mhash;
+    if (!history.pushState) {
+      return false;
+    }
+    mhash = [hash.zoom, hash.center[0], hash.center[1]];
+    if (!replace) {
+      history.pushState(hash, "", "#" + mhash.join("/"));
+    }
+    if (replace) {
+      history.replaceState(hash, "", "#" + mhash.join("/"));
+    }
+    return this.lastHash = hash;
+  };
+
+  L.Hash.prototype.parseHash = function(hash) {
+    var args, lat, lon, zoom;
+    if (hash.indexOf("#") === 0) {
+      hash = hash.substr(1);
+    }
+    args = hash.split("/");
+    if (args.length = 3) {
+      zoom = parseInt(args[0], 10);
+      lat = parseFloat(args[1]);
+      lon = parseFloat(args[2]);
+      if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
         return false;
-      }
-      mhash = this.getMapHash();
-      if (this.layerControl) {
-        layers = this.getLayers();
-      }
-      if (!this.layerControl) {
-        layers = {};
-      }
-      layers.zoom = mhash.shift();
-      layers.center = mhash;
-      hash = layers;
-      history.pushState(hash, "", this.makeHash(hash));
-      this.lastHash = hash;
-      return console.log(this.lastHash);
-    },
-    parseHash: function(hash) {
-      var args, lat, lon, zoom;
-      if (hash.indexOf("#") === 0) {
-        hash = hash.substr(1);
-      }
-      args = hash.split("/");
-      if (args.length >= 3) {
-        zoom = parseInt(args[0], 10);
-        lat = parseFloat(args[1]);
-        lon = parseFloat(args[2]);
-        if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
-          return false;
-        } else if (args.length === 3) {
-          return {
-            center: new L.LatLng(lat, lon),
-            zoom: zoom
-          };
-        } else {
-          if (args.length === 4) {
-            this.setBase(args[3]);
-          } else {
-            this.setBase(args[3]);
-            this.parseOver(args[4]);
-          }
-          return {
-            center: new L.LatLng(lat, lon),
-            zoom: zoom
-          };
-        }
       } else {
-        return false;
-      }
-    },
-    setBase: function(base) {
-      var baseName, i, len, name, parts;
-      baseName = decodeURIComponent(base);
-      len = this.layerControl._baseLayersList.childNodes.length;
-      i = 0;
-      while (i < len) {
-        parts = this.layerControl._baseLayersList.childNodes[i].childNodes;
-        name = parts[1].innerHTML.slice(1);
-        if (baseName === name) {
-          parts[0].setAttribute("checked", "checked");
-        }
-        i++;
-      }
-      this.layerControl._onInputClick();
-      return true;
-    },
-    parseOver: function(over) {
-      var layerControl, oArray;
-      if (over.indexOf("-") >= 0) {
-        oArray = over.split("-");
-      } else {
-        oArray = [over];
-      }
-      layerControl = this.layerControl;
-      oArray.forEach(function(oPart) {
-        var bName, eachNode, i, len, _results;
-        eachNode = function(v) {
-          var name, parts;
-          parts = v.childNodes;
-          name = parts[1].innerHTML.slice(1);
-          if (bName === name) {
-            return parts[0].checked = true;
-          } else {
-            return parts[0].checked = false;
-          }
+        return {
+          center: new L.LatLng(lat, lon),
+          zoom: zoom
         };
-        bName = decodeURIComponent(oPart);
-        len = layerControl._overlaysList.childNodes.length;
-        i = 0;
-        _results = [];
-        while (i < len) {
-          eachNode(layerControl._overlaysList.childNodes[i]);
-          _results.push(i++);
-        }
-        return _results;
-      });
-      return this.layerControl._onInputClick();
+      }
+    } else {
+      return false;
     }
   };
 
-  L.hash = function(map, layerControl) {
-    layerControl = layerControl || void 0;
-    return new L.Hash(map, layerControl);
+  L.Hash.prototype.update = function() {
+    return this.makeState(this.getHash());
+  };
+
+  L.Hash.prototype.remove = function() {
+    this.map.off("moveend", this.update);
+    window.onpopstate = null;
+    return location.hash = null;
+  };
+
+  L.hash = function(map) {
+    return new L.Hash(map);
+  };
+
+  L.Map.prototype.addHash = function() {
+    return L.hash(this);
+  };
+
+  L.Map.prototype.removeHash = function() {
+    return this._hash.remove();
   };
 
 }).call(this);
